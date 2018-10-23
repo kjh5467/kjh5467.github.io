f1640aed15cf4362c812f9e1abdf37797c49bb1c
unknown <khogan5467@gmail.com>
2018-10-23 00:26:58 -0400

    Updates

--- ghpage.js
+++
@@ -1,9 +0,0 @@
-var ghpages = require("gh-pages");
-
-/**
- * This task pushes to the `master` branch of the configured `repo`.
- */
-ghpages.publish("dist", {
-  branch: "master",
-  repo: "https://github.com/kjh5467/kjh5467.github.io"
-});
