48a74a3c1fe001377dacdcb703dec8592d2382e9
unknown <khogan5467@gmail.com>
2018-10-23 00:26:41 -0400

    removed callback from ghpage.js

--- ghpage.js
+++ ghpage.js
@@ -3,11 +3,7 @@ var ghpages = require("gh-pages");
 /**
  * This task pushes to the `master` branch of the configured `repo`.
  */
-ghpages.publish(
-  "dist",
-  {
-    branch: "master",
-    repo: "https://github.com/kjh5467/kjh5467.github.io"
-  },
-  callback
-);
+ghpages.publish("dist", {
+  branch: "master",
+  repo: "https://github.com/kjh5467/kjh5467.github.io"
+});
